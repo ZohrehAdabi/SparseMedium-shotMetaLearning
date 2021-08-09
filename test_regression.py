19cf3a1563653eb75f116572d2da88095db40871
ZohrehAdabi <adabi1458@gmail.com>
2021-08-09 22:37:27 +0430

    bug fixed

--- test_regression.py
+++ test_regression.py
@@ -37,7 +37,7 @@ elif params.method=='Sparse_DKT':
         if not os.path.isdir(params.checkpoint_dir):
             os.makedirs(params.checkpoint_dir)
         params.checkpoint_dir = params.checkpoint_dir +  f'KMeans_{str(params.n_centers)}'
-        print(params.checkpoint_dir)
+        # print(params.checkpoint_dir)
         model = Sparse_DKT(bb, k_means=k_means, n_inducing_points=params.n_centers, video_path=video_path, 
                             show_plots_pred=params.show_plots_pred, show_plots_features=params.show_plots_features, training=False).cuda()
     elif params.sparse_method=='FRVM':
