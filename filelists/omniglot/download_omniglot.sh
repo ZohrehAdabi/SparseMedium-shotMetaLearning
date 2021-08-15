4f89c68da2c3cdb0f91808056e57c7d4e5b52b7e
ZohrehAdabi <adabi1458@gmail.com>
2021-08-15 17:32:42 +0430

    add result

--- filelists/omniglot/download_omniglot.sh
+++ filelists/omniglot/download_omniglot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-# wget https://raw.githubusercontent.com/jakesnell/prototypical-networks/master/data/omniglot/splits/vinyals/train.txt 
-#wget https://raw.githubusercontent.com/jakesnell/prototypical-networks/master/data/omniglot/splits/vinyals/val.txt
-#wget https://raw.githubusercontent.com/jakesnell/prototypical-networks/master/data/omniglot/splits/vinyals/test.txt
+wget https://raw.githubusercontent.com/jakesnell/prototypical-networks/master/data/omniglot/splits/vinyals/train.txt 
+wget https://raw.githubusercontent.com/jakesnell/prototypical-networks/master/data/omniglot/splits/vinyals/val.txt
+wget https://raw.githubusercontent.com/jakesnell/prototypical-networks/master/data/omniglot/splits/vinyals/test.txt
 
 DATADIR=./images
 mkdir -p $DATADIR
