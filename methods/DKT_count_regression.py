1e3b57b9a340d82e3be1253bee07edbad7f2046b
Zohreh Adabi <adabi1458@gmail.com>
2021-08-24 10:44:19 +0430

    regression

--- methods/DKT_count_regression.py
+++ methods/DKT_count_regression.py
@@ -467,7 +467,7 @@ class DKT_count_regression(nn.Module):
             for i in range(r):
                 for j in range(c):
                 
-                    img = transforms.ToPILImage()(denormalize(x_q[k].cpu())).convert("RGB")
+                    img = transforms.ToPILImage()(denormalize(x_q[k]).cpu()).convert("RGB")
                     
                     plots = clear_ax(plots, i, j)
                     plots.ax[i, j].imshow(img)
