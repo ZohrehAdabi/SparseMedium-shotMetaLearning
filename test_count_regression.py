396ad33599325b3537ac3248fb1fc131e1b33344
Zohreh Adabi <adabi1458@gmail.com>
2021-08-24 09:34:53 +0430

    MSC44 DKT

--- test_count_regression.py
+++ test_count_regression.py
@@ -19,9 +19,9 @@ torch.backends.cudnn.benchmark = False
 
 params.checkpoint_dir = '%scheckpoints/%s/%s_%s' % (configs.save_dir, params.dataset, params.model, params.method)
 
-
+feat_map = 'map4'
 if  params.model=='ResNet50':
-    resnet50_conv, regressor = backbone.ResNet_Regrs()
+    resnet50_conv, regressor = backbone.ResNet_Regrs(feat_map)
     novel_file = configs.data_dir[params.dataset] + 'test.json'
 else:
     ValueError('Unknown model')
@@ -70,8 +70,19 @@ elif params.method=='Sparse_DKT':
 else:
     ValueError('Unrecognised method')
 
-model.load_checkpoint(params.checkpoint_dir)
-
+lr_gp = 1e-3
+lr_reg = 1e-5
+mse = True
+id = f'g_{lr_gp}_r_{lr_reg}_feat_{feat_map}'
+if mse: id = f'g_{lr_gp}_r_{lr_reg}_feat_{feat_map}_mse'
+best_models_list = os.listdir(f'/ave/checkpoints/{params.dataset}')
+if len(best_models_list) > 0:
+    best_mae = [best.split('_')[3] for best in best_models_list]
+    best_mae_idx = np.argmin(best_mae)
+    best_model = best_models_list[best_mae_idx]
+    model.load_checkpoint(os.path.join(configs.save_dir, params.dataset, best_model))
+else:
+    model.load_checkpoint(params.checkpoint_dir)
  
 mse_list = model.test(params.n_support, params.n_samples, optimizer, params.n_test_epochs)
 
