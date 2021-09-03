e0938cf83b6f90c8499e8ccdb692bc1729d2385a
Zohreh Adabi <adabi1458@gmail.com>
2021-09-03 16:53:24 +0430

    classification

--- methods/Sparse_DKT.py
+++ methods/Sparse_DKT.py
@@ -277,7 +277,7 @@ class Sparse_DKT(MetaTemplate):
         else:
             # with sigma and updating sigma converges to more sparse solution
             N   = inputs.shape[0]
-            tol = 1e-6
+            tol = 1e-12
             eps = torch.finfo(torch.float32).eps
             max_itr = 1000
             
