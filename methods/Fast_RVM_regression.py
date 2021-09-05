8b63b0496fe5c99031dc99d2b494c123c5bd771b
Zohreh Adabi <adabi1458@gmail.com>
2021-09-05 15:49:58 +0430

    classification

--- methods/Fast_RVM_regression.py
+++ methods/Fast_RVM_regression.py
@@ -290,9 +290,10 @@ def Fast_RVM_regression(K, targets, beta, N, config, align_thr, eps, tol, max_it
         if terminate:
             # print(f'sigma2={1/beta:.4f}')
             # if verbose:
-            print(f'Finished at {itr:3}, m= {active_m.shape[0]:3} sigma2= {1/beta:4.4f}')
-            if count > 0:
-                print(f'add: {add_count:3d} ({add_count/count:.1%}), delete: {del_count:3d} ({del_count/count:.1%}), recompute: {recomp_count:3d} ({recomp_count/count:.1%})')
+            if active_m.shape[0] < 3:
+                print(f'Finished at {itr:3}, m= {active_m.shape[0]:3} sigma2= {1/beta:4.4f}')
+            # if count > 0:
+            #     print(f'add: {add_count:3d} ({add_count/count:.1%}), delete: {del_count:3d} ({del_count/count:.1%}), recompute: {recomp_count:3d} ({recomp_count/count:.1%})')
             return active_m.cpu().numpy(), alpha_m, Gamma, beta 
 
         if ((itr+1)%50==0) and verbose:
