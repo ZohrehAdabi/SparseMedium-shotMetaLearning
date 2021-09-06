2a798a084be2ed47843c0566cabc38dbe5c81fe4
Zohreh Adabi <adabi1458@gmail.com>
2021-09-06 18:23:33 +0430

    classification

--- methods/Sparse_DKT_regression.py
+++ methods/Sparse_DKT_regression.py
@@ -272,7 +272,7 @@ class Sparse_DKT_regression(nn.Module):
                         best_mse = mse
                         # model_name = self.best_path + f'_best_mae{best_mse:.2f}_ep{epoch}_{id}.pth'
                         # self.save_checkpoint(model_name)
-                        print(Fore.LIGHTRED_EX, f'Best MSE: {best_mae:.2f}', Fore.RESET)
+                        print(Fore.LIGHTRED_EX, f'Best MSE: {best_mse:.2f}', Fore.RESET)
                 if(self.writer is not None) and self.show_plots_loss:
                     self.writer.add_scalar('MSE Val.', mse, epoch)
                 print(Fore.GREEN,"-"*30, Fore.RESET)
