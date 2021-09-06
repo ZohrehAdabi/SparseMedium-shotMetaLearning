c2b7c3abe1fba971fa888329b4713c94d878f1c4
Zohreh Adabi <adabi1458@gmail.com>
2021-09-07 01:53:09 +0430

    classification

--- methods/Fast_RVM_regression.py
+++ methods/Fast_RVM_regression.py
@@ -294,7 +294,6 @@ def Fast_RVM_regression(K, targets, beta, N, config, align_thr, eps, tol, max_it
                 mu_j			= mu_m[j]
                 mu_m			= mu_m + delta_mu.squeeze()
                 mu_m			= mu_m[torch.arange(mu_m.size(0)).to(device)!=j]
-                
                 jPm	            = (beta_KK_m @ s_j).squeeze()
                 S	            = S + jPm.pow(2) / s_jj
                 Q	            = Q + jPm * mu_j / s_jj
@@ -303,15 +302,22 @@ def Fast_RVM_regression(K, targets, beta, N, config, align_thr, eps, tol, max_it
                 KK_m            = KK[:, active_m]
                 KK_mm           = KK[active_m, :][:, active_m]
                 K_mt            = Kt[active_m]
-                beta_KK_m       = beta * KK_m
+                # beta_KK_m       = beta * KK_m
                 # update_required = True
-
-            if verbose:
-                    print(f'{itr:3}, update statistics after beta update')
-            Sigma_m, mu_m, S, Q, s, q, logML, Gamma = Statistics(K_m, KK_m, KK_mm, Kt, K_mt, alpha_m, active_m, beta, targets, N)
-            count = count + 1
+            count += 1
+            # s = S.clone()
+            # q = Q.clone()
+            # tmp = alpha_m / (alpha_m -S[active_m])
+            # s[active_m] = tmp * S[active_m] 
+            # q[active_m] = tmp * Q[active_m]
+            Sigma_m = Sigma_new
+            #quantity Gamma_i measures how well the corresponding parameter mu_i is determined by the data
+            Gamma = 1 - alpha_m * torch.diag(Sigma_m)
+            logML = logML + deltaLogMarginal
             logMarginalLog.append(logML.item())
-            terminate = False
+            beta_KK_m = beta * KK_m
+            
+            terminate = True
 
         #compute mu and beta
         if update_sigma and ((itr%5==0) or (itr <=10) or terminate):
