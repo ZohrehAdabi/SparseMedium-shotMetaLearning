57db70484476a662a2e11327aa727f1c9678d962
Zohreh Adabi <adabi1458@gmail.com>
2021-09-06 19:39:04 +0430

    classification

--- methods/Sparse_DKT_regression.py
+++ methods/Sparse_DKT_regression.py
@@ -29,6 +29,7 @@ from configs import kernel_type
 from collections import namedtuple
 #Check if tensorboardx is installed
 try:
+    #tensorboard --logdir=./QMUL_Loss/ --host localhost --port 8090
     from tensorboardX import SummaryWriter
     IS_TBX_INSTALLED = True
 except ImportError:
