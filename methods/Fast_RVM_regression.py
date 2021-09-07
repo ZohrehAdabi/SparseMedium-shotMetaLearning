27ca326829eec77cd6b1616e6f88165986a4a3c4
Zohreh Adabi <adabi1458@gmail.com>
2021-09-07 07:00:56 +0430

    classification

--- methods/Fast_RVM_regression.py
+++ methods/Fast_RVM_regression.py
@@ -346,8 +346,8 @@ def Fast_RVM_regression(K, targets, beta, N, config, align_thr, eps, tol, max_it
             # if verbose:
             # if active_m.shape[0] < 3:
             print(f'Finished at {itr:3}, m= {active_m.shape[0]:3} sigma2= {1/beta:4.4f}')
-            # if count > 0:
-            #     print(f'add: {add_count:3d} ({add_count/count:.1%}), delete: {del_count:3d} ({del_count/count:.1%}), recompute: {recomp_count:3d} ({recomp_count/count:.1%})')
+            if count > 0:
+                print(f'add: {add_count:3d} ({add_count/count:.1%}), delete: {del_count:3d} ({del_count/count:.1%}), recompute: {recomp_count:3d} ({recomp_count/count:.1%})')
             return active_m.cpu().numpy(), alpha_m, Gamma, beta, mu_m
 
         if ((itr+1)%50==0) and verbose:
@@ -355,6 +355,9 @@ def Fast_RVM_regression(K, targets, beta, N, config, align_thr, eps, tol, max_it
 
     if verbose:
         print(f'logML= {logML/N}\n{logMarginalLog}')
+    print(f'End, m= {active_m.shape[0]:3}')
+    if count > 0:
+        print(f'add: {add_count:3d} ({add_count/count:.1%}), delete: {del_count:3d} ({del_count/count:.1%}), recompute: {recomp_count:3d} ({recomp_count/count:.1%})')
 
     return active_m.cpu().numpy(), alpha_m, Gamma, beta, mu_m
 
