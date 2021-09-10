493b2e1a6a0b2f01060fca2ebb1b64ac04384dfe
ZohrehAdabi <adabi1458@gmail.com>
2021-09-10 07:54:25 +0430

    classification update

--- train_regression.py
+++ train_regression.py
@@ -93,8 +93,8 @@ elif params.method=='transfer':
 else:
     ValueError('Unrecognised method')
 
-optimizer = torch.optim.Adam([{'params': model.model.parameters(), 'lr': 0.003},
-                              {'params': model.feature_extractor.parameters(), 'lr': 0.0001}
+optimizer = torch.optim.Adam([{'params': model.model.parameters(), 'lr': 0.01}, #0.01
+                              {'params': model.feature_extractor.parameters(), 'lr': 0.001} #0.001
                               ])
 if params.method=='DKT' or params.method=='Sparse_DKT' or params.method=='DKT_New_Loss':
 
