16d4dfe3df95921d44d174fc84e2ce30c3fd38c7
Zohreh Adabi <adabi1458@gmail.com>
2022-01-02 16:34:16 +0330

    new loss

--- methods/Sparse_DKT_regression_Nystrom_new_loss.py
+++ methods/Sparse_DKT_regression_Nystrom_new_loss.py
@@ -32,7 +32,7 @@ from collections import namedtuple
 import torch.optim
 #Check if tensorboardx is installed
 try:
-    #tensorboard --logdir=./Sparse_DKT_QMUL_Nystrom_Loss/ --host localhost --port 8091
+    #tensorboard --logdir=./Sparse_DKT_Nystrom_QMUL_Loss/ --host localhost --port 8091
     from tensorboardX import SummaryWriter
     IS_TBX_INSTALLED = True
 except ImportError:
