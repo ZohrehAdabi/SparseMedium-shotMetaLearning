a33c1a6cd7e07d250c5b95a6d3cd6ca173b8d60b
Zohreh Adabi <adabi1458@gmail.com>
2022-01-07 20:35:32 +0330

    loss

--- methods/Sparse_DKT_regression_Nystrom.py
+++ methods/Sparse_DKT_regression_Nystrom.py
@@ -103,15 +103,15 @@ class Sparse_DKT_regression_Nystrom(nn.Module):
         
         N = targets.shape[0]
         targets = targets.to(torch.float64)
-        # K_mt = targets @ K_m
-        # A_m = torch.diag(alpha_m)
-        # H = A_m + beta * K_m.T @ K_m
-        # U, info =  torch.linalg.cholesky_ex(H, upper=True)
+        K_mt = targets @ K_m
+        A_m = torch.diag(alpha_m)
+        H = A_m + beta * K_m.T @ K_m
+        U, info =  torch.linalg.cholesky_ex(H, upper=True)
         # if info>0:
         #     print('pd_err of Hessian')
-        # U_inv = torch.linalg.inv(U)
-        # Sigma_m = U_inv @ U_inv.T      
-        # mu_m = beta * (Sigma_m @ K_mt)
+        U_inv = torch.linalg.inv(U)
+        Sigma_m = U_inv @ U_inv.T      
+        mu_m = beta * (Sigma_m @ K_mt)
         y_ = K_m @ mu_m  
         e = (targets - y_)
         ED = e.T @ e
@@ -125,9 +125,9 @@ class Sparse_DKT_regression_Nystrom(nn.Module):
         # C = sigma * I + K_m @ A_m @ K_m.T  ,  log|C| = - log|Sigma_m| - N * log(beta) - log|A_m|
         # t.T @ C^-1 @ t = beta * ||t - K_m @ mu_m||**2 + mu_m.T @ A_m @ mu_m 
         # log p(t) = -1/2 (log|C| + t.T @ C^-1 @ t ) + const 
-        # logML = -1/2 * (beta * ED + (mu_m**2) @ alpha_m )  #+ N * torch.log(beta) + 2*logdetHOver2
+        logML = -1/2 * (beta * ED + (mu_m**2) @ alpha_m )  #+ N * torch.log(beta) + 2*logdetHOver2
         # logML			= dataLikely - (mu_m**2) @ alpha_m /2 + torch.sum(torch.log(alpha_m))/2 - logdetHOver2
-        logML = -1/2 * beta * ED
+        # logML = -1/2 * beta * ED
     
         # NOTE new loss for rvm
         # S = torch.ones(N).to(self.device) *1/beta
