daa85a0a5c5983d6bcb2ed9eb38a81f23365a967
Zohreh Adabi <adabi1458@gmail.com>
2022-01-17 17:03:42 +0330

    save model

--- io_utils.py
+++ io_utils.py
@@ -50,7 +50,7 @@ def parse_args(script):
     parser.add_argument('--scale'               , action='store_true', help='normalize kernel matrix, K(z, z) in z space')
     if script == 'train':
         parser.add_argument('--num_classes'     , default=200, type=int, help='total number of classes in softmax, only used in baseline') #make it larger than the maximum label value in base class
-        parser.add_argument('--save_model'    , action='store_true', help='save model in some epochs.')
+        parser.add_argument('--save_model'      , action='store_true', help='save model in some epochs.')
         parser.add_argument('--save_freq'       , default=50, type=int, help='Save frequency')
         parser.add_argument('--start_epoch'     , default=0, type=int,help ='Starting epoch')
         parser.add_argument('--stop_epoch'      , default=-1, type=int, help ='Stopping epoch') #for meta-learning methods, each epoch contains 100 episodes. The default epoch number is dataset dependent. See train.py
