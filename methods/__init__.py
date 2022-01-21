03fd27b71208737f26d4cad332d69fd8e481ae3c
Zohreh Adabi <adabi1458@gmail.com>
2022-01-21 19:16:36 +0330

    edit

--- methods/__init__.py
+++ methods/__init__.py
@@ -9,6 +9,8 @@ from . import DKT, DKT_binary, DKT_regression
 from . import Sparse_DKT_Nystrom, Sparse_DKT_Exact, Sparse_DKT_binary_RVM
 from . import Sparse_DKT_binary_Nystrom, Sparse_DKT_binary_Exact, Sparse_DKT_binary_RVM
 from . import Sparse_DKT_regression_Nystrom, Sparse_DKT_regression_Exact, Sparse_DKT_regression_RVM
+import sys
+sys.path.append("..")
 from .. import configs
 
 
