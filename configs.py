3571b883886df8c83e43f8b737b7c97a90763252
Zohreh Adabi <adabi1458@gmail.com>
2022-01-22 00:35:51 +0330

    init

--- configs.py
+++ configs.py
@@ -10,6 +10,6 @@ data_dir['MSC44']          = './filelists/MSC44/'
 kernel_type                 = 'linear' #linear, rbf, 2rbf, spectral (regression only), matern, poli1, poli2, cossim, bncossim 
 run_float64 = True
 
-init_noise = 0.1
+init_noise = 0.05
 init_outputscale = 0.1
-init_lengthscale = 0.2
+init_lengthscale = 0.1
