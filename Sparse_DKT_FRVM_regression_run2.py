c5c861ccababe69a08e5225c6ae7098363b3df22
Zohreh Adabi <adabi1458@gmail.com>
2022-01-31 21:07:21 +0330

    exact reg

--- Sparse_DKT_FRVM_regression_run2.py
+++ Sparse_DKT_FRVM_regression_run2.py
@@ -12,8 +12,8 @@ stop_epoch = 100
 lr_gp_list = [0.001]
 lr_net_list = [0.001]
 config_list = ['1001']
-seed_list = [1, 2, 3]
-method_list = ['Sparse_DKT_Nystrom', 'Sparse_DKT_Exact']
+seed_list = [1, 2]
+method_list = ['Sparse_DKT_Exact']
 sparse_method = 'FRVM' # 'random'
 for config in config_list:
     for lr_gp in lr_gp_list:
@@ -34,7 +34,7 @@ for config in config_list:
                     ]
                     if save_model: L.append('--save_model')
                     print(f'\n{" ".join(L)} \n')
-                    # run(L)
+                    run(L)
                 
                 #DKT
                 L = ['python', f'./train_regression.py', 
@@ -60,7 +60,7 @@ for config in config_list:
                     ]
                     if save_model: L.append('--save_model')
                     print(f'\n{" ".join(L)} \n')
-                    run(L)
+                    # run(L)
 
                 for in_lr in [10]:
                     L = ['python', f'./train_regression.py', "--method","MAML", "--n_samples", "72",  "--n_support", "60", "--stop_epoch", f'{stop_epoch}', 
