a54058ae906f407bad8dbdfbce8c5d26f340ae38
Zohreh Adabi <adabi1458@gmail.com>
2022-02-01 23:55:31 +0330

    add_rvm_mll_one

--- methods/Sparse_DKT_binary_Nystrom.py
+++ methods/Sparse_DKT_binary_Nystrom.py
@@ -286,7 +286,7 @@ class Sparse_DKT_binary_Nystrom(MetaTemplate):
                     rvm_mll, _ = rvm_ML_regression(K_m, target, alpha_m, mu_m)
                 else:
                     rvm_mll = rvm_ML(K_m, target, alpha_m, mu_m, U)
-            elif self.add_rvm_mll:
+            elif self.add_rvm_mll or self.add_rvm_mll_one:
                 if self.regression:
                     rvm_mll, penalty = rvm_ML_regression_full(K_m, target, alpha_m, mu_m)
                 else:
