c16db9d1920faa03320298ca9c9bb8d9d8ad3752
Zohreh Adabi <adabi1458@gmail.com>
2022-02-11 15:20:01 +0330

    regre seed 4

--- Sparse_DKT_FRVM_regression_run.py
+++ Sparse_DKT_FRVM_regression_run.py
@@ -12,8 +12,8 @@ stop_epoch = 100
 lr_gp_list = [0.001]
 lr_net_list = [0.001]
 config_list = ['1001']
-seed_list = [1]
-method_list = ['Sparse_DKT_Exact'] #'Sparse_DKT_Nystrom', 
+seed_list = [4]
+method_list = ['Sparse_DKT_Nystrom','Sparse_DKT_Exact'] #
 sparse_method = 'FRVM' # 'random'
 for config in config_list:
     for lr_gp in lr_gp_list:
@@ -22,9 +22,34 @@ for config in config_list:
             if config in ['1000', '1010']:
                 align_thr = 0
             for sd in seed_list:
-                              
+                 #DKT
+                L = ['python', f'./train_regression.py', 
+                                '--method', f'DKT', '--n_samples', '72', '--n_support', '60', '--stop_epoch', f'{stop_epoch}', 
+                            #   '--show_plots_features',
+                                '--seed',  f'{sd}', 
+                                '--lr_gp',  f'{lr_gp}', '--lr_net',  f'{lr_net}',
+                                '--kernel_type', 'rbf', '--init'
+                ]
+                if save_model: L.append('--save_model')
+                print(f'\n{" ".join(L)} \n')
+                run(L)  
+
+                for method in method_list:
+                
+                    # just mll of GP
+                    L = ['python', f'./train_regression.py', 
+                                    '--method', f'{method}', '--sparse_method', f'{sparse_method}',  '--n_samples', '72', '--n_support', '60', '--stop_epoch', f'{stop_epoch}', 
+                                #   '--show_plots_features',
+                                    '--seed',  f'{sd}', '--config', f'{config}', '--align_thr', f'{align_thr}',  
+                                    '--lr_gp',  f'{lr_gp}', '--lr_net',  f'{lr_net}',
+                                    '--kernel_type', 'rbf', '--init',  '--beta'
+                    ]
+                    if save_model: L.append('--save_model')
+                    print(f'\n{" ".join(L)} \n')
+                    # run(L)
+                            
                    
-                lambda_rvm_list = [0.1, 0.5, 1.0]
+                lambda_rvm_list = [0.5, 1.0]
                 for lambda_rvm in lambda_rvm_list:
                     for method in method_list:    
                         
@@ -40,7 +65,7 @@ for config in config_list:
                         print(f'\n{" ".join(L)} \n')
                         run(L)
                     
-                lambda_rvm_list = [0.1, 0.5, 1.0]
+                lambda_rvm_list = [0.5, 1.0]
                 for lambda_rvm in lambda_rvm_list:
                     for method in method_list:
                         # rvm ll
@@ -58,13 +83,13 @@ for config in config_list:
                 L = ['python', f'./train_regression.py',  
                                         '--method', f'Sparse_DKT_RVM', '--sparse_method', f'{sparse_method}',  '--n_samples', '72', '--n_support', '60', '--stop_epoch', f'{stop_epoch}',
                                     #   '--show_plots_features',
-                                        '--seed',  f'{sd}', '--config', f'1001', '--align_thr', f'{align_thr}',  
+                                        '--seed',  f'{sd}', '--config', f'{config}', '--align_thr', f'{align_thr}',  
                                         '--lr_gp',  f'{lr_gp}', '--lr_net',  f'{lr_net}',
                                         '--kernel_type', 'rbf', '--init',  '--rvm_mll_only'
                         ]
                 if save_model: L.append('--save_model')
                 print(f'\n{" ".join(L)} \n')
-                # run(L)
+                run(L)
                 
        
 
