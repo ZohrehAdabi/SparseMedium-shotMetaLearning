47292ec75e26dd67dc50f249a689385960422c10
Zohreh Adabi <adabi1458@gmail.com>
2022-02-11 11:32:37 +0330

    CUB run

--- Sparse_DKT_FRVM_run.py
+++ Sparse_DKT_FRVM_run.py
@@ -12,8 +12,12 @@ lr_gp_list = [0.001]
 lr_net_list = [0.001]
 config_list = ['001']
 n_task = 20
-n_shot = 125 # 50
-n_query = 15 # 10
+if dataset=='CUB':
+    n_shot = 50 
+    n_query = 10 
+if dataset=='miniImagenet':
+    n_shot = 125 
+    n_query = 15
 
 tol_rvm = 1e-4
 max_itr = -1
@@ -21,7 +25,10 @@ sd = 1
 for config in config_list:
     for lr_gp in lr_gp_list:
         for lr_net in lr_net_list:
-            align_thr = 0.02 #0.03
+            if dataset=='CUB':
+                align_thr = 0.03 
+            if dataset=='miniImagenet':
+                align_thr = 0.02 
             if config in ['000', '010']:
                 align_thr = 0
             L = ['python', f'./train.py', 
@@ -34,7 +41,7 @@ for config in config_list:
                             "--train_aug"
             ]
             print(f'\n{" ".join(L)} \n')
-            # run(L)
+            run(L)
 
             
             for method in method_list:
@@ -50,7 +57,12 @@ for config in config_list:
                 print(f'\n{" ".join(L)} \n')
                 run(L)
 
-            lambda_rvm_list = [4.0]     
+     
+            if dataset=='CUB':
+                lambda_rvm_list = [2.0] 
+            if dataset=='miniImagenet':
+                lambda_rvm_list = [4.0] 
+
             for lambda_rvm in lambda_rvm_list:
                 for method in method_list:
                     L = ['python', f'./train.py', 
