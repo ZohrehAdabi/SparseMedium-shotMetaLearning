ad5f080dea46da68faf7cd1b9f252b2865eb93cc
Zohreh Adabi <adabi1458@gmail.com>
2022-02-11 20:33:59 +0330

    CUB run test 2

--- Sparse_DKT_FRVM_run_test.py
+++ Sparse_DKT_FRVM_run_test.py
@@ -6,22 +6,29 @@ method_list = ['Sparse_DKT_binary_Nystrom', 'Sparse_DKT_binary_Exact'] # , 'DKT_
 
 lr_gp_list = [0.1, 0.01, 0.001, 0.0001]
 lr_net_list = [0.01, 0.001, 0.0001]
-dataset =  'miniImagenet' #'miniImagenet' # 'CUB
+dataset =  'CUB' #'miniImagenet' # 'CUB
 lr_gp_list = [0.001]
 lr_net_list = [0.001]
 config_list = ['001']
 n_task = 20
-n_shot = 125 # 50
-n_query = 15 # 10
+if dataset=='CUB':
+    n_shot = 50 
+    n_query = 10 
+if dataset=='miniImagenet':
+    n_shot = 125 
+    n_query = 15
 
 tol_rvm = 1e-4
 max_itr = -1
-sd = 1
+sd = 2
 save_result = True
 for config in config_list:
     for lr_gp in lr_gp_list:
         for lr_net in lr_net_list:
-            align_thr = 0.02 # 0.03
+            if dataset=='CUB':
+                align_thr = 0.03 
+            if dataset=='miniImagenet':
+                align_thr = 0.02 
             if config in ['000', '010']:
                 align_thr = 0
             L = ['python', f'./test.py', 
@@ -51,7 +58,10 @@ for config in config_list:
                 print(f'\n{" ".join(L)} \n')
                 run(L)
 
-            lambda_rvm_list = [4.0]
+            if dataset=='CUB':
+                lambda_rvm_list = [2.0] 
+            if dataset=='miniImagenet':
+                lambda_rvm_list = [4.0] 
             for lambda_rvm in lambda_rvm_list:
                 for method in method_list:
                     L = ['python', f'./test.py', 
