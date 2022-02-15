a7fa2ff18853a1923c4bad443bd419413cb3dcef
Zohreh Adabi <adabi1458@gmail.com>
2022-02-15 07:42:48 +0330

    test clss ll

--- Sparse_DKT_FRVM_run_test.py
+++ Sparse_DKT_FRVM_run_test.py
@@ -20,7 +20,7 @@ if dataset=='miniImagenet':
 
 tol_rvm = 1e-4
 max_itr = -1
-sd = 3
+seed_list = [1, 2, 3]
 save_result = True
 for config in config_list:
     for lr_gp in lr_gp_list:
@@ -31,83 +31,85 @@ for config in config_list:
                 align_thr = 0.02 
             if config in ['000', '010']:
                 align_thr = 0
-            L = ['python', f'./test.py', 
-                        "--method","DKT_binary", "--dataset", f"{dataset}", 
-                        "--train_n_way", "2", "--test_n_way", "2", "--n_shot", f"{n_shot}", "--n_query", f"{n_query}",
-                            "--seed",  f"{sd}",  
-                            "--lr_gp", f"{lr_gp}", "--lr_net", f"{lr_net}", 
-                            '--kernel_type', 'linear', "--normalize", "--n_task",  f"{n_task}",
-                         
-                            "--train_aug"
-            ]
-            if save_result: L.append('--save_result')
-            print(f'\n{" ".join(L)} \n')
-            run(L)
 
-            for method in method_list:
+            for sd in seed_list:
                 L = ['python', f'./test.py', 
-                            "--method",f"{method}", "--sparse_method", "FRVM", "--dataset", f"{dataset}", 
+                            "--method","DKT_binary", "--dataset", f"{dataset}", 
                             "--train_n_way", "2", "--test_n_way", "2", "--n_shot", f"{n_shot}", "--n_query", f"{n_query}",
-                                "--seed",  f"{sd}", "--config", f"{config}", "--align_thr", f"{align_thr}" , 
+                                "--seed",  f"{sd}",  
                                 "--lr_gp", f"{lr_gp}", "--lr_net", f"{lr_net}", 
-                                '--kernel_type', 'linear', "--scale", "--normalize", "--n_task",  f"{n_task}",
-                                "--maxItr_rvm", f"{max_itr}", "--tol_rvm", f"{tol_rvm}", "--regression",
+                                '--kernel_type', 'linear', "--normalize", "--n_task",  f"{n_task}",
+                            
                                 "--train_aug"
                 ]
                 if save_result: L.append('--save_result')
                 print(f'\n{" ".join(L)} \n')
-                run(L)
+                # run(L)
 
-            if dataset=='CUB':
-                lambda_rvm_list = [2.0] 
-            if dataset=='miniImagenet':
-                lambda_rvm_list = [4.0] 
-            for lambda_rvm in lambda_rvm_list:
                 for method in method_list:
                     L = ['python', f'./test.py', 
                                 "--method",f"{method}", "--sparse_method", "FRVM", "--dataset", f"{dataset}", 
                                 "--train_n_way", "2", "--test_n_way", "2", "--n_shot", f"{n_shot}", "--n_query", f"{n_query}",
                                     "--seed",  f"{sd}", "--config", f"{config}", "--align_thr", f"{align_thr}" , 
                                     "--lr_gp", f"{lr_gp}", "--lr_net", f"{lr_net}", 
-                                    '--kernel_type', 'linear', "--scale", "--normalize",  "--n_task",  f"{n_task}",
-                                    "--regression", 
-                                    "--rvm_mll", "--lambda_rvm", f"{lambda_rvm}", "--maxItr_rvm", f"{max_itr}", "--tol_rvm", f"{tol_rvm}",
-                                    "--train_aug"
-                    ]
-                    if save_result: L.append('--save_result')
-                    print(f'\n{" ".join(L)} \n')
-                    run(L)
-
-
-            for lambda_rvm in lambda_rvm_list:
-                for method in method_list:
-                    L = ['python', f'./test.py', 
-                                "--method",f"{method}", "--sparse_method", "FRVM", "--dataset", f"{dataset}", 
-                                "--train_n_way", "2", "--test_n_way", "2", "--n_shot", f"{n_shot}", "--n_query", f"{n_query}",
-                                    "--seed",  f"{sd}", "--config", f"{config}", "--align_thr", f"{align_thr}" , 
-                                    "--lr_gp", f"{lr_gp}", "--lr_net", f"{lr_net}", 
-                                    '--kernel_type', 'linear', "--scale", "--normalize",  "--n_task", f"{n_task}",
-                                    "--regression", 
-                                    "--rvm_ll", "--lambda_rvm", f"{lambda_rvm}", "--maxItr_rvm", f"{max_itr}", "--tol_rvm", f"{tol_rvm}",
+                                    '--kernel_type', 'linear', "--scale", "--normalize", "--n_task",  f"{n_task}",
+                                    "--maxItr_rvm", f"{max_itr}", "--tol_rvm", f"{tol_rvm}", "--regression",
                                     "--train_aug"
                     ]
                     if save_result: L.append('--save_result')
                     print(f'\n{" ".join(L)} \n')
                     # run(L)
-             
+
+                if dataset=='CUB':
+                    lambda_rvm_list = [2.0] 
+                if dataset=='miniImagenet':
+                    lambda_rvm_list = [4.0] 
+                for lambda_rvm in lambda_rvm_list:
+                    for method in method_list:
+                        L = ['python', f'./test.py', 
+                                    "--method",f"{method}", "--sparse_method", "FRVM", "--dataset", f"{dataset}", 
+                                    "--train_n_way", "2", "--test_n_way", "2", "--n_shot", f"{n_shot}", "--n_query", f"{n_query}",
+                                        "--seed",  f"{sd}", "--config", f"{config}", "--align_thr", f"{align_thr}" , 
+                                        "--lr_gp", f"{lr_gp}", "--lr_net", f"{lr_net}", 
+                                        '--kernel_type', 'linear', "--scale", "--normalize",  "--n_task",  f"{n_task}",
+                                        "--regression", 
+                                        "--rvm_mll", "--lambda_rvm", f"{lambda_rvm}", "--maxItr_rvm", f"{max_itr}", "--tol_rvm", f"{tol_rvm}",
+                                        "--train_aug"
+                        ]
+                        if save_result: L.append('--save_result')
+                        print(f'\n{" ".join(L)} \n')
+                        # run(L)
+
+
+                for lambda_rvm in lambda_rvm_list:
+                    for method in method_list:
+                        L = ['python', f'./test.py', 
+                                    "--method",f"{method}", "--sparse_method", "FRVM", "--dataset", f"{dataset}", 
+                                    "--train_n_way", "2", "--test_n_way", "2", "--n_shot", f"{n_shot}", "--n_query", f"{n_query}",
+                                        "--seed",  f"{sd}", "--config", f"{config}", "--align_thr", f"{align_thr}" , 
+                                        "--lr_gp", f"{lr_gp}", "--lr_net", f"{lr_net}", 
+                                        '--kernel_type', 'linear', "--scale", "--normalize",  "--n_task", f"{n_task}",
+                                        "--regression", 
+                                        "--rvm_ll", "--lambda_rvm", f"{lambda_rvm}", "--maxItr_rvm", f"{max_itr}", "--tol_rvm", f"{tol_rvm}",
+                                        "--train_aug"
+                        ]
+                        if save_result: L.append('--save_result')
+                        print(f'\n{" ".join(L)} \n')
+                        run(L)
                 
-            L = ['python', f'./test.py', 
-                        "--method", "Sparse_DKT_binary_RVM", "--sparse_method", "FRVM", "--dataset", f"{dataset}", 
-                        "--train_n_way", "2", "--test_n_way", "2", "--n_shot", f"{n_shot}", "--n_query", f"{n_query}",
-                            "--seed",  f"{sd}", "--config", f"{config}", "--align_thr", f"{align_thr}" , 
-                            "--lr_gp", f"{lr_gp}", "--lr_net", f"{lr_net}", 
-                            '--kernel_type', 'linear', "--scale", "--normalize", "--n_task",  f"{n_task}",
-                            "--regression", 
-                            "--rvm_mll_only", "--maxItr_rvm", f"{max_itr}", "--tol_rvm", f"{tol_rvm}",
-                            "--train_aug"
-            ]
-            if save_result: L.append('--save_result')
-            print(f'\n{" ".join(L)} \n')
-            run(L)
+                    
+                L = ['python', f'./test.py', 
+                            "--method", "Sparse_DKT_binary_RVM", "--sparse_method", "FRVM", "--dataset", f"{dataset}", 
+                            "--train_n_way", "2", "--test_n_way", "2", "--n_shot", f"{n_shot}", "--n_query", f"{n_query}",
+                                "--seed",  f"{sd}", "--config", f"{config}", "--align_thr", f"{align_thr}" , 
+                                "--lr_gp", f"{lr_gp}", "--lr_net", f"{lr_net}", 
+                                '--kernel_type', 'linear', "--scale", "--normalize", "--n_task",  f"{n_task}",
+                                "--regression", 
+                                "--rvm_mll_only", "--maxItr_rvm", f"{max_itr}", "--tol_rvm", f"{tol_rvm}",
+                                "--train_aug"
+                ]
+                if save_result: L.append('--save_result')
+                print(f'\n{" ".join(L)} \n')
+                # run(L)
 
      
--- Sparse_DKT_FRVM_run_test2.py
+++ Sparse_DKT_FRVM_run_test2.py
@@ -10,12 +10,12 @@ lr_net_list = [0.01, 0.001, 0.0001]
 lr_gp_list = [0.001]
 lr_net_list = [0.001]
 config_list = ['001']
-sd = 2
+sd = 1
 save_result = True
 for config in config_list:
     for lr_gp in lr_gp_list:
         for lr_net in lr_net_list:
-            align_thr = 0.0015
+            align_thr = 0.002
             if config in ['000', '010']:
                 align_thr = 0
             L = ['python', f'./test.py', 
@@ -42,7 +42,9 @@ for config in config_list:
                 ]
                 if save_result: L.append('--save_result')
                 print(f'\n{" ".join(L)} \n')
-                run(L)
+                # run(L)
+
+
             lambda_rvm_list = [1.0]
             for lambda_rvm in lambda_rvm_list:
                 for method in method_list:
@@ -58,7 +60,7 @@ for config in config_list:
                     ]
                     if save_result: L.append('--save_result')
                     print(f'\n{" ".join(L)} \n')
-                    run(L)
+                    # run(L)
                 
             lambda_rvm_list = [1.0]
             for lambda_rvm in lambda_rvm_list:
@@ -75,7 +77,7 @@ for config in config_list:
                     ]
                     if save_result: L.append('--save_result')
                     print(f'\n{" ".join(L)} \n')
-                    # run(L)
+                    run(L)
 
             L = ['python', f'./test.py', 
                                 "--method", f"Sparse_DKT_RVM", "--sparse_method", "FRVM", "--dataset", "omniglot", 
@@ -89,28 +91,5 @@ for config in config_list:
                     ]
             if save_result: L.append('--save_result')
             print(f'\n{" ".join(L)} \n')
-            run(L)
-            # run(['python', f'./train.py', 
-            #             "--method","Sparse_DKT_binary_Nystrom", "--sparse_method", "FRVM", "--dataset", "omniglot", 
-            #             "--train_n_way", "2", "--test_n_way", "2", "--n_shot", "15", "--n_query", "5",
-            #                 "--seed","1", "--config", f"{config}", "--align_thr", "1e-3" , 
-            #                 "--lr_gp", f"{lr_gp}", "--lr_net", f"{lr_net}", "--gamma" 
-            #                 #,"--train_aug"
-            # ])
-# config_list = ['000', '001', '010', '011']
-# lr_gp_list = [ 0.01, 0.001]
-# lr_net_list = [ 0.001, 0.0001]
-# for config in config_list:
-#     for lr_gp in lr_gp_list:
-#         for lr_net in lr_net_list:
-#             align_thr = 1e-3
-#             if config in ['000', '010']:
-#                 align_thr = 0
-            
-#             run(['python', f'./train.py', 
-#                         "--method","Sparse_DKT_binary_Nystrom", "--sparse_method", "FRVM", "--dataset", "CUB", 
-#                         "--train_n_way", "2", "--test_n_way", "2", "--n_shot", "50", "--n_query", "10",
-#                             "--seed","1", "--config", f"{config}", "--align_thr", f"{align_thr}" , 
-#                             "--lr_gp", f"{lr_gp}", "--lr_net", f"{lr_net}"
-#                             #,"--train_aug"
-#             ])
+            # run(L)
+     
