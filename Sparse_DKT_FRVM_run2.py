b5e8451634cd7c2c8aafde03a7ec3b645ad66f63
Zohreh Adabi <adabi1458@gmail.com>
2022-02-23 23:20:10 +0330

    omniglot

--- Sparse_DKT_FRVM_run2.py
+++ Sparse_DKT_FRVM_run2.py
@@ -15,7 +15,7 @@ sd = 3
 for config in config_list:
     for lr_gp in lr_gp_list:
         for lr_net in lr_net_list:
-            align_thr = 0.002
+            align_thr = 0.001
             if config in ['000', '010']:
                 align_thr = 0
             L = ['python', f'./train.py', 
@@ -29,23 +29,23 @@ for config in config_list:
             print(f'\n{" ".join(L)} \n')
             # run(L)
             
-            # method_list = ['Sparse_DKT_Exact']
-            lambda_rvm_list = [1.0]
-            # for align_thr in [0.001, 0.002, 0.0025, 0.003]:
-            for lambda_rvm in lambda_rvm_list:
-                for method in method_list:
-                    L = ['python', f'./train.py', 
-                                "--method", f"{method}", "--sparse_method", "FRVM", "--dataset", "omniglot", 
-                                "--train_n_way", "5", "--test_n_way", "5", "--n_shot", "15", "--n_query", "5",
-                                    "--seed",  f"{sd}", "--config", f"{config}", "--align_thr", f"{align_thr}" , 
-                                    "--lr_gp", f"{lr_gp}", "--lr_net", f"{lr_net}", "--stop_epoch", "100",
-                                    '--kernel_type', 'linear', "--scale", "--save_model", "--n_task", "30",
-                                    "--regression", 
-                                    "--rvm_mll", "--lambda_rvm", f"{lambda_rvm}"
-                                
-                    ]
-                    print(f'\n{" ".join(L)} \n')
-                    run(L)  
+            method_list = ['Sparse_DKT_Exact']
+            lambda_rvm_list = [0.1, 1.0]
+            for align_thr in [0.0, 0.0008, 0.001, 0.002]:
+                for lambda_rvm in lambda_rvm_list:
+                    for method in method_list:
+                        L = ['python', f'./train.py', 
+                                    "--method", f"{method}", "--sparse_method", "FRVM", "--dataset", "omniglot", 
+                                    "--train_n_way", "5", "--test_n_way", "5", "--n_shot", "15", "--n_query", "5",
+                                        "--seed",  f"{sd}", "--config", f"{config}", "--align_thr", f"{align_thr}" , 
+                                        "--lr_gp", f"{lr_gp}", "--lr_net", f"{lr_net}", "--stop_epoch", "100",
+                                        '--kernel_type', 'linear', "--scale", "--save_model", "--n_task", "30",
+                                        "--regression", 
+                                        "--rvm_mll", "--lambda_rvm", f"{lambda_rvm}"
+                                    
+                        ]
+                        print(f'\n{" ".join(L)} \n')
+                        run(L)  
 
 
             for method in method_list:
@@ -62,7 +62,7 @@ for config in config_list:
                 # run(L) 
                 
             # rvm ll
-            lambda_rvm_list = [1.0]
+            lambda_rvm_list = [0.1, ]
             for lambda_rvm in lambda_rvm_list:
                 for method in method_list:
                     L = ['python', f'./train.py', 
@@ -76,7 +76,7 @@ for config in config_list:
                                 
                     ]
                     print(f'\n{" ".join(L)} \n')
-                    run(L)
+                    # run(L)
 
             L = ['python', f'./train.py', 
                                 "--method", f"Sparse_DKT_RVM", "--sparse_method", "FRVM", "--dataset", "omniglot", 
@@ -90,27 +90,4 @@ for config in config_list:
                     ]
             print(f'\n{" ".join(L)} \n')
             # run(L)
-            # run(['python', f'./train.py', 
-            #             "--method","Sparse_DKT_binary_Nystrom", "--sparse_method", "FRVM", "--dataset", "omniglot", 
-            #             "--train_n_way", "2", "--test_n_way", "2", "--n_shot", "15", "--n_query", "5",
-            #                 "--seed","1", "--config", f"{config}", "--align_thr", "1e-3" , 
-            #                 "--lr_gp", f"{lr_gp}", "--lr_net", f"{lr_net}", "--gamma" 
-            #                 #,"--train_aug"
-            # ])
-# config_list = ['000', '001', '010', '011']
-# lr_gp_list = [ 0.01, 0.001]
-# lr_net_list = [ 0.001, 0.0001]
-# for config in config_list:
-#     for lr_gp in lr_gp_list:
-#         for lr_net in lr_net_list:
-#             align_thr = 1e-3
-#             if config in ['000', '010']:
-#                 align_thr = 0
-            
-#             run(['python', f'./train.py', 
-#                         "--method","Sparse_DKT_binary_Nystrom", "--sparse_method", "FRVM", "--dataset", "CUB", 
-#                         "--train_n_way", "2", "--test_n_way", "2", "--n_shot", "50", "--n_query", "10",
-#                             "--seed","1", "--config", f"{config}", "--align_thr", f"{align_thr}" , 
-#                             "--lr_gp", f"{lr_gp}", "--lr_net", f"{lr_net}"
-#                             #,"--train_aug"
-#             ])
+     
