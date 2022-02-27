8c66d5476b68a371b6536d9f13064f8c9d572ac9
Zohreh Adabi <adabi1458@gmail.com>
2022-02-27 22:00:48 +0330

    MetaOptNet binary

--- Sparse_DKT_FRVM_run_test2.py
+++ Sparse_DKT_FRVM_run_test2.py
@@ -10,7 +10,7 @@ lr_net_list = [0.01, 0.001, 0.0001]
 lr_gp_list = [0.001]
 lr_net_list = [0.001]
 config_list = ['001']
-seed_list = [3]
+seed_list = [1, 2]
 method_list = ['Sparse_DKT_Exact']
 save_result = True
 for config in config_list:
