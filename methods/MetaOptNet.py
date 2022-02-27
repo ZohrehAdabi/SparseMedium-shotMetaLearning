cba7b8d38b55bcea1ce7eb80da88642dcb5ffd39
Zohreh Adabi <adabi1458@gmail.com>
2022-02-27 22:48:04 +0330

    MetaOptNet

--- methods/MetaOptNet.py
+++ methods/MetaOptNet.py
@@ -60,7 +60,7 @@ class MetaOptNet(MetaTemplate):
         # optimizer = torch.optim.Adam([{'params': self.model.parameters(), 'lr': 1e-4},
         #                               {'params': self.feature_extractor.parameters(), 'lr': 1e-3}])
 
-        update = 4
+        update = 2
         loss_list = []
         for i, (x,_) in enumerate(train_loader):
             self.n_query = x.size(1) - self.n_support
