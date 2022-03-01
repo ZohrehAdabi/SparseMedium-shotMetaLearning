35440f76e20d0882daec58919665c9680e284dbb
Zohreh Adabi <adabi1458@gmail.com>
2022-03-01 05:46:24 +0330

    MAML CUB, miniImg

--- Sparse_DKT_FRVM_run.py
+++ Sparse_DKT_FRVM_run.py
@@ -143,7 +143,7 @@ for config in config_list:
                             "--method","MAML", "--dataset", "CUB", 
                             "--train_n_way", "2", "--test_n_way", "2", "--n_shot", "50", "--n_query", "10",
                                 "--seed",  f"{sd}",  
-                                "--lr_net", f"{lr_net}", "--inner_lr", f"{inner_lr}", '--inner_loop', '5',
+                                "--lr_net", f"{lr_net}", "--inner_lr", f"{inner_lr}", '--inner_loop', '10', '--first_order',
                                 "--stop_epoch", "100",
                                 "--save_model", "--n_task", "20",  '--train_aug', '--normalize',
                             
@@ -156,7 +156,7 @@ for config in config_list:
                             "--method","MAML", "--dataset", "miniImagenet", 
                             "--train_n_way", "2", "--test_n_way", "2", "--n_shot", "125", "--n_query", "15",
                                 "--seed",  f"{sd}",  
-                                "--lr_net", f"{lr_net}", "--inner_lr", f"{inner_lr}", '--inner_loop', '5',
+                                "--lr_net", f"{lr_net}", "--inner_lr", f"{inner_lr}", '--inner_loop', '10', '--first_order',
                                 "--stop_epoch", "100",
                                 "--save_model", "--n_task", "20",  '--train_aug', '--normalize',
                             
