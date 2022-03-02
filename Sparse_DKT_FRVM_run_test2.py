f49fcd07df9fefb5f2342c6a1a07bf1679d9fa1a
Zohreh Adabi <adabi1458@gmail.com>
2022-03-02 08:24:28 +0330

    MAML omniglot

--- Sparse_DKT_FRVM_run_test2.py
+++ Sparse_DKT_FRVM_run_test2.py
@@ -104,7 +104,7 @@ for config in config_list:
                             "--method","MAML", "--dataset", "omniglot", 
                             "--train_n_way", "5", "--test_n_way", "5", "--n_shot", "15", "--n_query", "5",
                                 "--seed",  f"{sd}",  
-                                "--lr_net", f"{lr_net}", "--inner_lr", f"{inner_lr}", '--inner_loop', '10',
+                                "--lr_net", f"{lr_net}", "--inner_lr", f"{inner_lr}", '--inner_loop', '10', '--first_order',
                                 
                                 "--n_task", "30"
                             
