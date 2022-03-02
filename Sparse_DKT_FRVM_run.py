96c225bf28b5b9d5f0be4356485a1f8e45e5cb31
Zohreh Adabi <adabi1458@gmail.com>
2022-03-02 22:52:35 +0330

    CUM MAML

--- Sparse_DKT_FRVM_run.py
+++ Sparse_DKT_FRVM_run.py
@@ -162,16 +162,16 @@ for config in config_list:
                             
                 ]
                 print(f'\n{" ".join(L)} \n')
-                run(L)
+                # run(L)
 
             L = ['python', f'./Sparse_DKT_FRVM_run_test.py'
             ]
             print(f'\n{" ".join(L)} \n')
             run(L)
 
-            L = ['python', f'./Sparse_DKT_FRVM_run2.py'
-            ]
-            print(f'\n{" ".join(L)} \n')
-            run(L)
+            # L = ['python', f'./Sparse_DKT_FRVM_run2.py'
+            # ]
+            # print(f'\n{" ".join(L)} \n')
+            # run(L)
 
            
