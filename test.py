9dd6e69e9271902f859a8d2cbd4200e7ed6b20c3
Zohreh Adabi <adabi1458@gmail.com>
2022-06-30 05:54:32 +0430

    test 2 way --

--- test.py
+++ test.py
@@ -84,8 +84,8 @@ def single_test(params):
 
     few_shot_params = dict(n_way = params.test_n_way , n_support = params.n_shot)
 
-    print('2 way') 
-    few_shot_params = dict(n_way = 2 , n_support = params.n_shot) 
+    # print('2 way') 
+    # few_shot_params = dict(n_way = 2 , n_support = params.n_shot) 
 
     if params.dataset in ['omniglot', 'cross_char']:
         assert params.model == 'Conv4' and not params.train_aug ,'omniglot only support Conv4 without augmentation'
